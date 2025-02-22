4cb5b96649cac603217cb5aa9b63facac19bce2a
yerassyl667 <kulseitov06@mail.ru>
2025-02-22 10:53:25 +0500

    sem

---
+++ self/modules.py
@@ -0,0 +1,10 @@
+class triangle:
+    def __init__(self, base, height):
+        self.base = base
+        self.height = height 
+    def area(self):
+        return (self.base * self.height) / 2 
+    
+
+
+    
